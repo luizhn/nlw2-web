b9e6e6e4a64103ef33c64709078f772919ea9566
Luiz Nascimento <luiz.nascimento@redemelhorcompra.com.br>
2020-08-06 20:01:14 -0300

    feat: criando componente page header

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
 
 // import { Container } from './styles';
 
-const TeacherForm: React.FC = () => {
-  return <div />;
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar Aula." />
+    </div>);
 }
 
 export default TeacherForm;
